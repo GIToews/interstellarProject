802de29259bfd4ff5cfbc3eed403ab49b8d10f46
Joshua Toews <joshptoews@gmail.com>
2017-11-16 15:05:28 +0000

    Added star_life_cycle validations and recieved 26% on milestone 2

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,14 +1,7 @@
 class Passenger < ApplicationRecord
 
-
   validates :user_name, :name, :password, presence: true
   validates :user_name, uniqueness: true
-  # validates :seats, numericality: true
-  # validates :from, :to, length: {minimum: 2}
-  # validates :from, :to, length: {maximum: 12}
-
-
-
 
   has_many :flight_passengers
   has_many :flights, through: :flight_passengers
--- app/models/star.rb
+++ app/models/star.rb
@@ -1,7 +1,11 @@
 class Star < ApplicationRecord
+
+  valid_stars = ['Average Star', 'Red Giant', 'White Dwarf', 'Brown Dwarf', 'Massive Star', 'Red Super Giant', 'Neutron Star', 'Black Hole']
+
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :name, length: {minimum: 3}
+  validates :life_cycle_stage, inclusion: { in: valid_stars, message: "%{value} is not a valid star life cycle stage" }
 
   has_many :destinations
 end
